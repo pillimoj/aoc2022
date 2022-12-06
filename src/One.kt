a55c9f54e6d2da21469a8df683dbe05596321fd7
Eric Grimsborn <eric.grimsborn@netlight.com>
2022-12-06 12:21:46 +0100

    day one

--- src/One.kt
+++ src/One.kt
@@ -1,11 +1,28 @@
 package aoc
 
 object One : Day(1) {
+
+    private val calorieSizes: List<Int> = run {
+        val calorieSizes = buildList {
+            var currentCalories = 0
+            this@One.puzzleInput.forEach {
+                if (it.isBlank()) {
+                    this.add(currentCalories)
+                    currentCalories = 0
+                } else {
+                    currentCalories += it.toInt()
+                }
+            }
+        }
+        calorieSizes
+    }
     override fun a(): String {
-        return "not done"
+        val result = calorieSizes.maxOrNull()!!
+        return "the largest number of calories is $result"
     }
 
     override fun b(): String {
-        return "not done"
+        val topThree = calorieSizes.sortedDescending().take(3).sum()
+        return "the sum of top three calorie sizes is $topThree"
     }
 }
